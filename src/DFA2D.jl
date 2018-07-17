019e362de09dae9f10f8908a8df6e64c9e88db5f
Corentin Nélias <34754896+johncwok@users.noreply.github.com>
2018-07-17 13:10:14 +0200

    Update DFA2D.jl

--- src/DFA2D.jl
+++ src/DFA2D.jl
@@ -3,7 +3,7 @@ module DFA2D
 using PyPlot
 include("calculs.jl")
 
-function DFA(data1,data2,box_b::Int,box_s::Int,nb_pt::Int,plot::Boolean)
+function DFA(data1,data2,box_b::Int,box_s::Int,nb_pt::Int,plot::Bool)
     x =  log_space(box_b,box_s,nb_pt)
     y = fluctuation_function(data1,data2,box_b,box_s,nb_pt; fit_type = "polynomial")
     if plot == false 
