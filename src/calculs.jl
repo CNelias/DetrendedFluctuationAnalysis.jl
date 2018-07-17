684081a1c5c2e84785303c6ec6f70099124d91fb
Corentin Nélias <34754896+johncwok@users.noreply.github.com>
2018-07-17 13:18:16 +0200

    Update calculs.jl

--- src/calculs.jl
+++ src/calculs.jl
@@ -1,5 +1,6 @@
-export fluctuation_function
+export fluctuation_function, DFA, log_space
 using Polynomials
+using PyPlot
 
 function log_space(start::Int,stop::Int,num::Int)
     tmp = map(x -> round(Int,x), logspace(log10(start),log10(stop),num))
@@ -61,4 +62,23 @@ function fluctuation_function(x,y, box_start::Int, box_stop::Int, nb_pts::Int; f
         append!(ff,1/n*sqrt(abs(ffi)))
     end
     return ff
+end
+
+function DFA(data1,data2,box_b::Int,box_s::Int,nb_pt::Int,plot::Bool)
+    x =  log_space(box_b,box_s,nb_pt)
+    y = fluctuation_function(data1,data2,box_b,box_s,nb_pt; fit_type = "polynomial")
+    if plot == false 
+      return y
+    elseif plot == true
+      plot(x,y,"bo-",markersize = 4, label = "DFA of data"); 
+      a = linreg(log.(x),log.(y))[2]
+      b = linreg(log.(x),log.(y))[1]      
+      title("DFA_2D analysis")
+      legend()
+      xscale("log")
+      yscale("log")
+      xlabel("Log(s)")
+      ylabel("Log(F(n))")
+      plot(x,exp(b)*x.^a, color = "black",linestyle = ":", label = string(L"fit. exponant $\alpha$ = ", a))
+    end
 end
