c58654a9ffd2992558eb5295738c97408f727dc6
Corentin Nélias <34754896+johncwok@users.noreply.github.com>
2018-07-19 09:52:37 +0200

    removing pyplot dependance

---
+++ src/DetrendedFluctuationAnalysis.jl.jl
@@ -0,0 +1,13 @@
+module DFA2D
+
+include("calculs.jl")
+
+function DFA(data1,data2,box_b::Int,box_s::Int,nb_pt::Int,plot::Bool)
+    x =  log_space(box_b,box_s,nb_pt)
+    y = fluctuation_function(data1,data2,box_b,box_s,nb_pt; fit_type = "polynomial")
+    return x,y
+end
+
+export DFA
+
+end
